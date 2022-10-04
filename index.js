c94c2c4f658834746e1977ab33b80805f073cdf3
FieryInferno <bagassetia271@gmail.com>
2022-10-04 20:46:46 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const corsOptions = {origin: '*'};
+const bodyParser = require('body-parser');
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(5000, () => console.log('Server is running on port 5000'));
+
+app.get('/catch', (req, res) => {
+  return res.status(200).send({
+    catchStatus: Math.floor(Math.random() * 2),
+  });
+})
+app.get('/release', (req, res) => {
+  return res.status(200).send({
+    releaseStatus: Math.floor(Math.random() * 101),
+  });
+})
+app.get('/rename/:n', (req, res) => {
+  const {n} = req.params;
+
+  return res.status(200).send({
+    fibonacci: fibonacci(parseInt(n)),
+    rename: parseInt(n) + 1,
+  });
+})
+
+const fibonacci = (num) => {
+  if(num < 2) {
+    return num;
+  }
+  else {
+    return fibonacci(num-1) + fibonacci(num - 2);
+  }
+}
