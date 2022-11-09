d6826c10fd4b17ff14a41379af57a1a4c9a3d6f9
FieryInferno <bagassetia271@gmail.com>
2022-11-09 20:36:13 +0700

    fix: port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log('Server is running on port 5000'));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.get('/', (req, res) => {
   return res.status(200).send('Welcome');
 })
