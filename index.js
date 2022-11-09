fff210ef9e825ee318ed126bc25f7d17e7ab1ed2
FieryInferno <bagassetia271@gmail.com>
2022-11-09 20:33:14 +0700

    fix: Change port

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(5000, () => console.log('Server is running on port 5000'));
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server is running on port 5000'));
 app.get('/', (req, res) => {
   return res.status(200).send('Welcome');
 })
